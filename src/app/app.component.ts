d6b3ee9da86a4ee40a647df394c474964360981c
Kong310544 <kong310544@gmail.com>
2021-11-23 16:54:14 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'BS2';
+  n1 : number = 5;
+  n2 : number = 2;
+  picName:string = "https://images.pexels.com/photos/850602/pexels-photo-850602.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260";
+
+  hi(name:string){
+    alert("Hi" + name);
+  }
+}
